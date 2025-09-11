aae46b5705b93d530b6f904a4cf547ad3a615f4a
abubakarcode-play <abubakarshahidinn786@gmail.com>
2025-09-11 18:53:04 +0500

    Add files via upload

---
+++ couintrycode.py
@@ -0,0 +1,9 @@
+country_code = {'India': '0091',
+                   'Pakistan': '0092',
+                   'Australia': '00977'}
+
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
+
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))
