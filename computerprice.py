f8f77258a636281ec6c4744345769ce9100d3f1a
abubakarcode-play <abubakarshahidinn786@gmail.com>
2025-10-08 17:28:38 +0500

    Add files via upload

---
+++ computerprice.py
@@ -0,0 +1,16 @@
+class Computer:
+
+    def __init__(self):
+        self.__maxprice = 900
+    
+    def sell(self):
+        print("Selling Price: {}".format(self.__maxprice))
+
+c = Computer()
+c.sell()
+
+c.__maxprice = 1000
+c.sell()
+
+c.setMaxPrice(1000)
+c.sell()
